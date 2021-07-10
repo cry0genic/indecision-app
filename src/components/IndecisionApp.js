8cd7c67ea1c81c0d89fd81c6c41f8ebe3f6aaf81
cry0genic <cry0genic1103@gmail.com>
2021-07-10 17:06:23 +0530

    css

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -65,23 +65,29 @@ export default class IndecisionApp extends React.Component {
     }
     
     render() {
-      const subtitle = 'Put your life in the hands of a computer';
+      const subtitle = 'Put your life in the hands of a computer.';
       const title = 'Indecision App'
       return (
         <div>
           <Header title={title}  subtitle={subtitle} />
-          <Action
+          <div className='container'>
+            <Action
             hasOptions={this.state.options.length > 0}
             handlePick={this.handlePick}
-          />
-          <Options
-            options={this.state.options}
-            handleDeleteOptions={this.handleDeleteOptions}
-            handleDeleteOption={this.handleDeleteOption}
-          />
-          <AddOption
-            handleAddOption={this.handleAddOption}
-          />
+            />
+            <div className='widget'>
+              <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+              />
+              <AddOption
+              handleAddOption={this.handleAddOption}
+              />
+            </div>
+            
+          </div>
+          
           <OptionModal selectedOption={this.state.selectedOption}
             handleClearSelectedOption={this.handleClearSelectedOption}
           />
